6109fa6bbe3acabeddf2847de87cabf9ccd5e59b
vtsukur <flushdia@gmail.com>
2015-06-02 00:28:37 +0300

    Listing Offer properties in the right order.

--- src/main/java/org/vtsukur/spring/rest/market/ApplicationConfiguration.java
+++ src/main/java/org/vtsukur/spring/rest/market/ApplicationConfiguration.java
@@ -1,15 +1,12 @@
 package org.vtsukur.spring.rest.market;
 
 import com.fasterxml.jackson.databind.Module;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JSR310Module;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
-import org.springframework.data.repository.init.Jackson2RepositoryPopulatorFactoryBean;
-
-import static com.fasterxml.jackson.databind.DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES;
+import org.vtsukur.spring.rest.market.util.CustomRepositoryPopulator;
 
 /**
  * @author volodymyr.tsukur
@@ -18,15 +15,14 @@ import static com.fasterxml.jackson.databind.DeserializationFeature.FAIL_ON_UNKN
 public class ApplicationConfiguration {
 
     @Bean
-    public Jackson2RepositoryPopulatorFactoryBean repositoryPopulator() {
-        Jackson2RepositoryPopulatorFactoryBean factory = new Jackson2RepositoryPopulatorFactoryBean();
-        ObjectMapper mapper = new ObjectMapper();
-        mapper.configure(FAIL_ON_UNKNOWN_PROPERTIES, false);
-        mapper.registerModule(newJSR310Module());
-        factory.setMapper(mapper);
+    public CustomRepositoryPopulator repositoryPopulator() {
+        CustomRepositoryPopulator factory = new CustomRepositoryPopulator();
+//        ObjectMapper mapper = new ObjectMapper();
+//        mapper.configure(FAIL_ON_UNKNOWN_PROPERTIES, false);
+//        mapper.registerModule(newJSR310Module());
+//        factory.setMapper(mapper);
         factory.setResources(new Resource[]{
-                new ClassPathResource("user-data.json"),
-                new ClassPathResource("offer-data.json")
+                new ClassPathResource("data.json")
         });
         return factory;
     }
