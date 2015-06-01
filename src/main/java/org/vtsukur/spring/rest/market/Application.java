d3d90ad67d71e5104fed57b3fc2273614c88b8c8
glvtsuku <volodymyr.tsukur@avid.com>
2015-06-01 17:08:40 +0300

    Providing both user and offer data.

--- src/main/java/org/vtsukur/spring/rest/market/Application.java
+++ src/main/java/org/vtsukur/spring/rest/market/Application.java
@@ -1,5 +1,8 @@
 package org.vtsukur.spring.rest.market;
 
+import com.fasterxml.jackson.databind.Module;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JSR310Module;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -7,21 +10,35 @@ import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.data.repository.init.Jackson2RepositoryPopulatorFactoryBean;
 
+import static com.fasterxml.jackson.databind.DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES;
+
 /**
  * @author volodymyr.tsukur
  */
 @SpringBootApplication
 public class Application {
 
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
     @Bean
     public Jackson2RepositoryPopulatorFactoryBean repositoryPopulator() {
         Jackson2RepositoryPopulatorFactoryBean factory = new Jackson2RepositoryPopulatorFactoryBean();
-        factory.setResources(new Resource[] { new ClassPathResource("data.json") });
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.configure(FAIL_ON_UNKNOWN_PROPERTIES, false);
+        mapper.registerModule(newJSR310Module());
+        factory.setMapper(mapper);
+        factory.setResources(new Resource[]{
+                new ClassPathResource("user-data.json"),
+                new ClassPathResource("offer-data.json")
+        });
         return factory;
     }
 
-    public static void main(final String[] args) {
-        SpringApplication.run(Application.class, args);
+    @Bean
+    public Module newJSR310Module() {
+        return new JSR310Module();
     }
 
 }
