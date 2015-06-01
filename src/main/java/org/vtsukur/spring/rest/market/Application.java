a5ec816520da5397e766eb6faa1147103171f51d
vtsukur <flushdia@gmail.com>
2015-06-01 15:07:49 +0300

    Initial check-in.

---
+++ src/main/java/org/vtsukur/spring/rest/market/Application.java
@@ -0,0 +1,16 @@
+package org.vtsukur.spring.rest.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author volodymyr.tsukur
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
