a2fa85077f3a9d0a4921084d078c9378d6a93975
vtsukur <flushdia@gmail.com>
2015-06-01 18:26:45 +0300

    Exposing users via CrudRepository to show variation in collection representations.

--- src/main/java/org/vtsukur/spring/rest/market/domain/user/UserRepository.java
+++ src/main/java/org/vtsukur/spring/rest/market/domain/user/UserRepository.java
@@ -1,9 +1,9 @@
 package org.vtsukur.spring.rest.market.domain.user;
 
-import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.CrudRepository;
 
 /**
  * @author volodymyr.tsukur
  */
-public interface UserRepository extends PagingAndSortingRepository<User, Long> {
+public interface UserRepository extends CrudRepository<User, Long> {
 }
