548b3b031cddcce9af4d1f836a78b8404ee74cbe
vtsukur <flushdia@gmail.com>
2015-06-01 15:29:31 +0300

    Adding Spring Data REST and BaseEntity.

---
+++ src/main/java/org/vtsukur/spring/rest/market/domain/BaseEntity.java
@@ -0,0 +1,23 @@
+package org.vtsukur.spring.rest.market.domain;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import org.springframework.hateoas.Identifiable;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+/**
+ * @author volodymyr.tsukur
+ */
+@MappedSuperclass
+@Getter
+@EqualsAndHashCode
+public abstract class BaseEntity implements Identifiable<Long> {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+}
