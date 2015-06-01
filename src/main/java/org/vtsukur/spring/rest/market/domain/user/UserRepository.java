2a047cd39d89fc1ef89c7aab03ee10123eb3dc8d
vtsukur <flushdia@gmail.com>
2015-06-02 00:47:46 +0300

    User repository must not be exported.

--- src/main/java/org/vtsukur/spring/rest/market/domain/user/UserRepository.java
+++ src/main/java/org/vtsukur/spring/rest/market/domain/user/UserRepository.java
@@ -1,9 +1,11 @@
 package org.vtsukur.spring.rest.market.domain.user;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 /**
  * @author volodymyr.tsukur
  */
+@RestResource(exported = false)
 public interface UserRepository extends CrudRepository<User, Long> {
 }
