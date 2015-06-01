b4bdcd1b022097391543f37f03331b1597a8ac7d
glvtsuku <volodymyr.tsukur@avid.com>
2015-06-01 17:10:20 +0300

    Moving application configuration to a separate class.

--- src/main/java/org/vtsukur/spring/rest/market/Application.java
+++ src/main/java/org/vtsukur/spring/rest/market/Application.java
@@ -1,16 +1,7 @@
 package org.vtsukur.spring.rest.market;
 
-import com.fasterxml.jackson.databind.Module;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.jsr310.JSR310Module;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
-import org.springframework.data.repository.init.Jackson2RepositoryPopulatorFactoryBean;
-
-import static com.fasterxml.jackson.databind.DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES;
 
 /**
  * @author volodymyr.tsukur
@@ -20,25 +11,6 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-    }
-
-    @Bean
-    public Jackson2RepositoryPopulatorFactoryBean repositoryPopulator() {
-        Jackson2RepositoryPopulatorFactoryBean factory = new Jackson2RepositoryPopulatorFactoryBean();
-        ObjectMapper mapper = new ObjectMapper();
-        mapper.configure(FAIL_ON_UNKNOWN_PROPERTIES, false);
-        mapper.registerModule(newJSR310Module());
-        factory.setMapper(mapper);
-        factory.setResources(new Resource[]{
-                new ClassPathResource("user-data.json"),
-                new ClassPathResource("offer-data.json")
-        });
-        return factory;
-    }
-
-    @Bean
-    public Module newJSR310Module() {
-        return new JSR310Module();
     }
 
 }
