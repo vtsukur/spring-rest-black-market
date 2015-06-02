455acad1d5a3c5fa486ea8f38b0391358752ec6e
vtsukur <flushdia@gmail.com>
2015-06-02 10:43:41 +0300

    Changing direction of the User -> Ad to Ad -> User.

--- src/main/java/org/vtsukur/spring/rest/market/domain/core/ad/Ad.java
+++ src/main/java/org/vtsukur/spring/rest/market/domain/core/ad/Ad.java
@@ -3,10 +3,13 @@ package org.vtsukur.spring.rest.market.domain.core.ad;
 import lombok.Getter;
 import lombok.Setter;
 import org.vtsukur.spring.rest.market.domain.core.BaseEntity;
+import org.vtsukur.spring.rest.market.domain.core.user.User;
 import org.vtsukur.spring.rest.market.infrastructure.LocalDateTimeConverter;
 
 import javax.persistence.Convert;
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.time.LocalDateTime;
@@ -18,6 +21,10 @@ import java.time.LocalDateTime;
 @Getter
 @Setter
 public class Ad extends BaseEntity {
+
+    @ManyToOne
+    @JoinColumn(nullable = false)
+    private User user;
 
     @Convert(converter = LocalDateTimeConverter.class)
     private LocalDateTime publishedAt;
