a9969669b1a56fc3f5160ab64596ae4bab93421a
vtsukur <flushdia@gmail.com>
2015-06-03 19:41:56 +0300

    Adding query method to select ads by user.

--- src/main/java/org/vtsukur/spring/rest/market/domain/core/ad/AdRepository.java
+++ src/main/java/org/vtsukur/spring/rest/market/domain/core/ad/AdRepository.java
@@ -1,9 +1,18 @@
 package org.vtsukur.spring.rest.market.domain.core.ad;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RestResource;
+import org.vtsukur.spring.rest.market.domain.core.user.User;
 
 /**
  * @author volodymyr.tsukur
  */
 public interface AdRepository extends PagingAndSortingRepository<Ad, Long> {
+
+    @RestResource(rel = "by-user", path = "byUser")
+    Page<Ad> findByUser(@Param("user") User user, Pageable pageable);
+
 }
