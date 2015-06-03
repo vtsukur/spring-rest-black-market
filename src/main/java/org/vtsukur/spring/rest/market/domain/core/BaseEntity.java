69cf1ce85a9dfe167ccb305943f1843e0a68094d
vtsukur <flushdia@gmail.com>
2015-06-03 21:16:39 +0300

    Adding toString generation to BaseEntity.

--- src/main/java/org/vtsukur/spring/rest/market/domain/core/BaseEntity.java
+++ src/main/java/org/vtsukur/spring/rest/market/domain/core/BaseEntity.java
@@ -2,6 +2,7 @@ package org.vtsukur.spring.rest.market.domain.core;
 
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.ToString;
 import org.springframework.hateoas.Identifiable;
 
 import javax.persistence.GeneratedValue;
@@ -13,6 +14,7 @@ import javax.persistence.MappedSuperclass;
  */
 @MappedSuperclass
 @Getter
+@ToString
 @EqualsAndHashCode
 public abstract class BaseEntity implements Identifiable<Long> {
 
