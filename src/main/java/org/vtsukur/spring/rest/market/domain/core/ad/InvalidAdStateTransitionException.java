a900236b372f9d93ba0686b879687cd62df72868
vtsukur <flushdia@gmail.com>
2015-06-03 21:17:52 +0300

    Moving core of the publishing logic to domain entity and a service. Introducing failure case.

---
+++ src/main/java/org/vtsukur/spring/rest/market/domain/core/ad/InvalidAdStateTransitionException.java
@@ -0,0 +1,16 @@
+package org.vtsukur.spring.rest.market.domain.core.ad;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+/**
+ * @author volodymyr.tsukur
+ */
+@ResponseStatus(HttpStatus.CONFLICT)
+public class InvalidAdStateTransitionException extends RuntimeException {
+
+    public InvalidAdStateTransitionException() {
+        super();
+    }
+
+}
