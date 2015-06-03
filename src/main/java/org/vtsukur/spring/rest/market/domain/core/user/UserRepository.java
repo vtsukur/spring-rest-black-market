7e8747342daec3802922369868d6036673d6a2c1
vtsukur <flushdia@gmail.com>
2015-06-03 20:05:46 +0300

    Simplifying the job by making user repository exposed for now.

--- src/main/java/org/vtsukur/spring/rest/market/domain/core/user/UserRepository.java
+++ src/main/java/org/vtsukur/spring/rest/market/domain/core/user/UserRepository.java
@@ -1,11 +1,9 @@
 package org.vtsukur.spring.rest.market.domain.core.user;
 
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.rest.core.annotation.RestResource;
 
 /**
  * @author volodymyr.tsukur
  */
-@RestResource(exported = false)
 public interface UserRepository extends CrudRepository<User, Long> {
 }
