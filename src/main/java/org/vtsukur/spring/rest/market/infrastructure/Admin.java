5e89ab304df3254126c729366fc6116ddc32fb1e
vtsukur <flushdia@gmail.com>
2015-06-04 22:45:47 +0300

    Adding an Admin.

---
+++ src/main/java/org/vtsukur/spring/rest/market/infrastructure/Admin.java
@@ -0,0 +1,12 @@
+package org.vtsukur.spring.rest.market.infrastructure;
+
+/**
+ * @author volodymyr.tsukur
+ */
+public final class Admin {
+
+    public static final String HONTAREVA = "hontareva";
+
+    private Admin() {}
+
+}
