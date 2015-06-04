29135b3afc2c9fc27834f0875760619fd6eb2e3c
vtsukur <flushdia@gmail.com>
2015-06-04 22:49:00 +0300

    Securing user repository operations to be accessible to administrators only (except of the new findByPhoneNumber call).

--- src/main/java/org/vtsukur/spring/rest/market/domain/core/user/UserRepository.java
+++ src/main/java/org/vtsukur/spring/rest/market/domain/core/user/UserRepository.java
@@ -1,11 +1,59 @@
 package org.vtsukur.spring.rest.market.domain.core.user;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.security.access.prepost.PreAuthorize;
 
 /**
  * @author volodymyr.tsukur
  */
-@PreAuthorize("hasRole('ROLE_ADMIN')")
 public interface UserRepository extends CrudRepository<User, Long> {
+
+    @RestResource(exported = false)
+    User findByPhoneNumber(String phoneNumber);
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @Override
+    <S extends User> S save(S entity);
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @Override
+    <S extends User> Iterable<S> save(Iterable<S> entities);
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @Override
+    User findOne(Long aLong);
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @Override
+    boolean exists(Long aLong);
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @Override
+    Iterable<User> findAll();
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @Override
+    Iterable<User> findAll(Iterable<Long> longs);
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @Override
+    long count();
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @Override
+    void delete(Long aLong);
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @Override
+    void delete(User entity);
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @Override
+    void delete(Iterable<? extends User> entities);
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @Override
+    void deleteAll();
+
 }
