13d46fd4dbe8f3b206ec25982fa968ad8cf24bee
vtsukur <flushdia@gmail.com>
2015-06-04 20:58:47 +0300

    Any user operations should require ADMIN role.

--- src/main/java/org/vtsukur/spring/rest/market/domain/core/user/UserRepository.java
+++ src/main/java/org/vtsukur/spring/rest/market/domain/core/user/UserRepository.java
@@ -1,9 +1,11 @@
 package org.vtsukur.spring.rest.market.domain.core.user;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.security.access.prepost.PreAuthorize;
 
 /**
  * @author volodymyr.tsukur
  */
+@PreAuthorize("hasRole('ROLE_ADMIN')")
 public interface UserRepository extends CrudRepository<User, Long> {
 }
