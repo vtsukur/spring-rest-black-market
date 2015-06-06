cbc635d7d45efe5bd40aa0c1d30a4b31655aba95
vtsukur <flushdia@gmail.com>
2015-06-06 13:30:37 +0300

    Removing even repositories.

--- src/main/java/org/vtsukur/spring/rest/market/domain/core/ad/AdRepository.java
+++
@@ -1,9 +0,0 @@
-package org.vtsukur.spring.rest.market.domain.core.ad;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-/**
- * @author volodymyr.tsukur
- */
-public interface AdRepository extends PagingAndSortingRepository<Ad, Long> {
-}
--- src/main/java/org/vtsukur/spring/rest/market/domain/core/user/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package org.vtsukur.spring.rest.market.domain.core.user;
-
-import org.springframework.data.repository.CrudRepository;
-
-/**
- * @author volodymyr.tsukur
- */
-public interface UserRepository extends CrudRepository<User, Long> {
-}
