6f8d0869c1d4056330cac3cefb24ee1f3158870d
vtsukur <flushdia@gmail.com>
2015-06-07 17:41:50 +0300

    No need to set response status initially - it is better to demo it.

--- src/main/java/org/vtsukur/spring/rest/market/domain/core/ad/InvalidAdStateTransitionException.java
+++ src/main/java/org/vtsukur/spring/rest/market/domain/core/ad/InvalidAdStateTransitionException.java
@@ -1,12 +1,8 @@
 package org.vtsukur.spring.rest.market.domain.core.ad;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
 /**
  * @author volodymyr.tsukur
  */
-@ResponseStatus(HttpStatus.CONFLICT)
 public class InvalidAdStateTransitionException extends RuntimeException {
 
     public InvalidAdStateTransitionException(String message) {
