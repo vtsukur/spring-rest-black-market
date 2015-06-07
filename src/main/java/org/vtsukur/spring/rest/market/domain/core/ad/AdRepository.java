73a5068bfe256dfaead9109e7618eefef57f6035
vtsukur <flushdia@gmail.com>
2015-06-08 00:14:17 +0300

    Removing methods which are no longer used.

--- src/main/java/org/vtsukur/spring/rest/market/domain/core/ad/AdRepository.java
+++ src/main/java/org/vtsukur/spring/rest/market/domain/core/ad/AdRepository.java
@@ -7,7 +7,6 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
-import org.vtsukur.spring.rest.market.domain.core.user.User;
 
 /**
  * @author volodymyr.tsukur
@@ -17,12 +16,6 @@ public interface AdRepository extends PagingAndSortingRepository<Ad, Long> {
 
     @RestResource(rel = "by", path = "by")
     Page<Ad> findByTypeAndCurrency(@Param("type") Ad.Type type, @Param("currency") Ad.Currency currency, Pageable pageable);
-
-    @RestResource(rel = "by-user", path = "by-user")
-    Page<Ad> findByUser(@Param("value") User user, Pageable pageable);
-
-    @RestResource(rel = "by-user-phonenumber", path = "by-user-phonenumber")
-    Page<Ad> findByUser_PhoneNumber(@Param("value") String phoneNumber, Pageable pageable);
 
     @RestResource(rel = "my", path = "my")
     @Query("select ad from Ad ad where ad.user.phoneNumber = ?#{ principal?.username }")
