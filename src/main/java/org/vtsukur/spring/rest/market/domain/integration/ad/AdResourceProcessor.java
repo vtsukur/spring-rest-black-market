2a42b6db6635bad24152fbfe7aa58008a308e8d7
glvtsuku <volodymyr.tsukur@avid.com>
2015-06-09 16:20:02 +0300

    Advanced security rules - links are available only if user has 'write' access to them (= his ads).

--- src/main/java/org/vtsukur/spring/rest/market/domain/integration/ad/AdResourceProcessor.java
+++ src/main/java/org/vtsukur/spring/rest/market/domain/integration/ad/AdResourceProcessor.java
@@ -4,8 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.EntityLinks;
 import org.springframework.hateoas.Resource;
 import org.springframework.hateoas.ResourceProcessor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.vtsukur.spring.rest.market.domain.core.ad.Ad;
+import org.vtsukur.spring.rest.market.infrastructure.CustomUserDetailsService;
 
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
@@ -22,15 +25,26 @@ public class AdResourceProcessor implements ResourceProcessor<Resource<Ad>> {
     @Override
     public Resource<Ad> process(Resource<Ad> resource) {
         Ad ad = resource.getContent();
-        if (ad.getStatus() == Ad.Status.NEW) {
-            resource.add(entityLinks.linkToSingleResource(ad).withRel("update"));
-            resource.add(entityLinks.linkToSingleResource(ad).withRel("delete"));
-            resource.add(linkTo(methodOn(AdResourceController.class).publish(ad.getId(), null)).withRel("publish"));
-        }
-        if (ad.getStatus() == Ad.Status.PUBLISHED) {
-            resource.add(linkTo(methodOn(AdResourceController.class).finish(ad.getId(), null)).withRel("finish"));
+        if (hasAccessToModify(ad)) {
+            if (ad.getStatus() == Ad.Status.NEW) {
+                resource.add(entityLinks.linkToSingleResource(ad).withRel("update"));
+                resource.add(entityLinks.linkToSingleResource(ad).withRel("delete"));
+                resource.add(linkTo(methodOn(AdResourceController.class).publish(ad.getId(), null)).withRel("publish"));
+            }
+            if (ad.getStatus() == Ad.Status.PUBLISHED) {
+                resource.add(linkTo(methodOn(AdResourceController.class).finish(ad.getId(), null)).withRel("finish"));
+            }
         }
         return resource;
+    }
+
+    private static boolean hasAccessToModify(Ad ad) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth == null) {
+            return false;
+        }
+        CustomUserDetailsService.CustomUserDetails principal = (CustomUserDetailsService.CustomUserDetails) auth.getPrincipal();
+        return principal != null && ad.getUser().getId().equals(principal.getId());
     }
 
 }
