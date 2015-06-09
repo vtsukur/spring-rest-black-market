3302a1fb749476aab27b0e62c77e5e9fdbbbb800
vtsukur <flushdia@gmail.com>
2015-06-09 15:58:47 +0300

    Removing excerpt to make sure that links will be generated.

--- src/main/java/org/vtsukur/spring/rest/market/domain/core/ad/AdRepository.java
+++ src/main/java/org/vtsukur/spring/rest/market/domain/core/ad/AdRepository.java
@@ -5,13 +5,11 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
 
 /**
  * @author volodymyr.tsukur
  */
-@RepositoryRestResource(excerptProjection = AdExcerpt.class)
 public interface AdRepository extends PagingAndSortingRepository<Ad, Long> {
 
     @RestResource(rel = "by", path = "by")
