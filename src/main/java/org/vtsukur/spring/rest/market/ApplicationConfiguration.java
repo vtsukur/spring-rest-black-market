412eb31e2bb8c0e87a0aa12e86c6f06e6b44f1be
vtsukur <flushdia@gmail.com>
2015-06-17 19:13:46 +0300

    Bringing back CurieProvider.

--- src/main/java/org/vtsukur/spring/rest/market/ApplicationConfiguration.java
+++ src/main/java/org/vtsukur/spring/rest/market/ApplicationConfiguration.java
@@ -8,6 +8,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration;
+import org.springframework.hateoas.UriTemplate;
+import org.springframework.hateoas.hal.CurieProvider;
+import org.springframework.hateoas.hal.DefaultCurieProvider;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -267,6 +270,11 @@ public class ApplicationConfiguration extends WebSecurityConfigurerAdapter {
     @Bean
     public Module newJSR310Module() {
         return new JSR310Module();
+    }
+
+    @Bean
+    public CurieProvider curieProvider() {
+        return new DefaultCurieProvider("currency-black-market", new UriTemplate("http://localhost:8080/alps/{rel}"));
     }
 
     @Configuration
