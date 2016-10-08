b887dbd937f74e272e3bb5e0df1cc0d69ec81fec
vtsukur <flushdia@gmail.com>
2016-10-08 20:24:23 +0300

    Removing commented lines

--- src/main/java/org/vtsukur/spring/rest/market/domain/core/ad/AdRepository.java
+++ src/main/java/org/vtsukur/spring/rest/market/domain/core/ad/AdRepository.java
@@ -10,7 +10,6 @@ import org.springframework.data.rest.core.annotation.RestResource;
 /**
  * @author volodymyr.tsukur
  */
-//@RepositoryRestResource(excerptProjection = StandardAdProjection.class)
 public interface AdRepository extends PagingAndSortingRepository<Ad, Long> {
 
     @RestResource(rel = "published", path = "published")
